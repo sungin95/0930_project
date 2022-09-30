d2df81d6e2ef30431d82d35442a059bdf56f5193
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-09-30 16:50:17 +0900

    html bootstrap

--- pair/review/views.py
+++ pair/review/views.py
@@ -1,12 +1,27 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from .models import Review
+import random
 
 # Create your views here.
 def index(request):
+    img_list = [
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+        "https://static.wikia.nocookie.net/pokemon/images/3/3f/%EC%9D%B4%EB%B8%8C%EC%9D%B4_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest/scale-to-width-down/200?cb=20170405085011&path-prefix=ko",
+        "https://static.wikia.nocookie.net/pokemon/images/c/c8/%EB%A6%AC%EC%9E%90%EB%AA%BD_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest/scale-to-width-down/250?cb=20170404233220&path-prefix=ko",
+    ]
+
     review = Review.objects.all()
+    count = Review.objects.count()
+    images = []
+
+    for i in range(count):
+        image = random.choice(img_list)
+        images.append(image)
+
     context = {
         "reviews": review,
+        "images": images,
     }
     return render(request, "review/index.html", context)
 
